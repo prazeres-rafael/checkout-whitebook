f2f8908bfd36692dda1feb7d5ec2401e5e483ad9
Rafael Prazeres <rafaelpraz.adm@gmail.com>
2024-01-12 00:31:38 -0300

    fix: create a global theme style for mui
    this commit creates a global theme style for mui using corretly font.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 import CheckoutWhitebook from "@/features/CheckoutWhitebook";
 
+const customThemeFont = createTheme({
+  typography: {
+    fontFamily: `'__DM_Sans_6ff133', '__DM_Sans_Fallback_6ff133';`,
+  },
+});
+
 const Home = () => {
-  return <CheckoutWhitebook />;
+  return (
+    <ThemeProvider theme={customThemeFont}>
+      <CheckoutWhitebook />
+    </ThemeProvider>
+  );
 };
 
 export default Home;
